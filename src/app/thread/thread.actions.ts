2208eaa7e4981e2cdfe633bf253178150e936f05
Alex2588 <alexander_beschasnyy@mail.ru>
2017-05-18 19:10:31 +0300

    test commit

--- src/app/thread/thread.actions.ts
+++ src/app/thread/thread.actions.ts
@@ -1,3 +1,8 @@
+import { Action, ActionCreator } from '@ngrx/store';
+import uuid from 'uuid';
+import { Thread } from './thread.model';
+import { Message } from '../message/message.model';
+
 export const ADD_THREAD = '[Thread] Add';
 
 export interface AddThreadAction extends Action {
@@ -6,5 +11,41 @@ export interface AddThreadAction extends Action {
 
 export const addThread: ActionCreator<AddThreadAction> = thread => ({
     type: ADD_THREAD,
+    thread
+});
+
+export const ADD_MESSAGE = '[Message] Add Message';
+
+export interface AddMessageAction extends Action {
+    thread: Thread,
+    message: Message
+}
+
+export const addMessage: ActionCreator<AddMessageAction> = (thread: Thread, messageArs: Message): AddMessageAction => {
+    
+    const defaults = {
+        id: uuid(),
+        sentAt: new Date(),
+        isRead: false,
+        thread
+    };
+    
+    const message: Message = Object.assign({}, defaults, messageArs);
+    
+    return {
+        type: ADD_MESSAGE,
+        thread,
+        message
+    }
+};
+
+export const SELECT_THREAD = '[Thread] Select';
+
+export interface SelectThreadAction extends Action {
+    thread: Thread
+}
+
+export const selectThread: ActionCreator<SelectThreadAction> = thread => ({
+    type: SELECT_THREAD,
     thread
 });
--- src/app/thread/threads.reducer.ts
+++ src/app/thread/threads.reducer.ts
@@ -1,3 +1,7 @@
+import { Action } from '@ngrx/store';
+import * as ThreadActions from './thread.actions';
+import { Thread } from './thread.model';
+
 export interface ThreadsEntities {
     [id: string]: Thread
 }
@@ -13,3 +17,54 @@ const initialState: ThreadsState = {
     entities: {},
     currentThreadId: null
 } 
+
+export const ThreadsReducer = function (state: ThreadsState = initialState, action: Action): ThreadsState {
+    
+    switch(action.type) {
+        
+        case ThreadActions.ADD_THREAD:
+            const thread = (<ThreadActions.AddThreadAction>action).thread;
+            if(state.ids.includes(thread.id)) {
+                return state;
+            }
+
+            return {
+                ids: [...state.ids, thread.id],
+                currentThreadId: state.currentThreadId,
+                entities: Object.assign({}, state.entities, { [thread.id]: thread })
+            }
+
+        case ThreadActions.ADD_MESSAGE:
+            const thread = (<ThreadActions.AddMessageAction>action).thread;
+            const message = (<ThreadActions.AddMessageAction>action).message;            
+
+            const isRead = message.thread.id === state.currentThreadId? true : message.isRead;
+            const newMessage = Object.assign({}, message, {isRead: isRead});
+
+            const oldThread = state.entities[thread.id];
+
+            const newThread = Object.assign({}, oldThread, { messages: [...oldThread.messages, newMessage] });
+
+            return {
+                ids: state.ids,
+                currentThreadId: state.currentThreadId,
+                entities: Object.assign({}, state.entities, { [thread.id]: newThread })
+            }
+
+        case ThreadActions.SELECT_THREAD:
+            const thread = (<ThreadActions.SelectThreadAction>action).thread;
+            const oldThread = state.entities[thread.id];
+
+            const newMessages = oldThread.messages.map(message => Object.assign({}, message, { isRead: true }));
+            const newThread = Object.assign({}, oldThread, { messages: newMessage });
+
+            return {
+                ids: state.ids,
+                currentThreadId: thread.id,
+                entities: Object.assign({}, state.entities, { [thread.id]: newThread })
+            }
+
+        default:
+            return state;
+    }
+}
